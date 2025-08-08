4df471d877537b78ee7e4c14e8a3b7120cc4b100
Eugene <157493723+eugene-IT-creator@users.noreply.github.com>
2025-08-08 13:17:38 +0200

    cat moved more to the right

--- game/Cat.js
+++ game/Cat.js
@@ -13,7 +13,7 @@ export default class Cat {
         this.maxJumpHeight = maxJumpHeight;
         this.scaleRatio = scaleRatio;
 
-        this.x = 10 * scaleRatio;
+        this.x = 30 * scaleRatio;
         this.y = this.canvas.height - this.height - 1.5 * scaleRatio;
         this.yInitial = this.y;
         this.image = new Image();
